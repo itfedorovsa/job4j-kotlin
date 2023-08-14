792feb8419b14098471856a060241a4bc0094368
FedorovSA <itfedorovsa@gmail.com>
2023-08-14 14:56:08 +0300

    Version compatibility fix (try)

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,6 @@ tasks.test {
 }
 
 java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
