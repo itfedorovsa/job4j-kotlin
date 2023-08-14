7282ea683a90982d10375d824208b40988ff5428
FedorovSA <itfedorovsa@gmail.com>
2023-08-14 12:54:27 +0300

    Init commit. HelloWorld

---
+++ README.md
@@ -0,0 +1,12 @@
+# job4j_kotlin
+
+# About
+
+This project contains various Kotlin lessons and tasks 
+
+***
+
+# Contact
+
+Contact me if you have any questions: itfedorovsa@gmail.com
+
---
+++ src/main/java/ru/job4j/base/HelloWorld.java
@@ -0,0 +1,8 @@
+package ru.job4j.base;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello from Kotlin");
+        HelloWorldKt.main(args);
+    }
+}
