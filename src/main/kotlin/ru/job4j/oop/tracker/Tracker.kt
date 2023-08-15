7664d3e77eb18da25bfe1e9f740fc987554bc6b9
FedorovSA <itfedorovsa@gmail.com>
2023-08-15 17:31:36 +0300

    Classes

---
+++ src/main/kotlin/ru/job4j/oop/tracker/Tracker.kt
@@ -0,0 +1,26 @@
+package ru.job4j.oop.tracker
+
+data class Tracker(private var ids: Int = 1, private val items: ArrayList<Item> = ArrayList()) {
+
+    fun add(item: Item): Item {
+        item.id = ids++
+        items.add(item)
+        return item
+    }
+
+    fun findById(id: Int): Item? {
+        var rsl: Item? = null
+        for (name in items) {
+            val item: Item = name
+            if (item.id == id) {
+                rsl = item
+                break
+            }
+        }
+        return rsl
+    }
+
+    fun findAll(): List<Item> {
+        return items.toList()
+    }
+}
---
+++ src/test/kotlin/ru/job4j/oop/tracker/TrackerTest.kt
@@ -0,0 +1,29 @@
+package ru.job4j.oop.tracker
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class TrackerTest {
+
+    @Test
+    fun whenAdd() {
+        val tracker = Tracker()
+        tracker.add(Item(0, "ItemName"))
+        assertEquals(tracker.findAll().size, 1)
+    }
+
+    @Test
+    fun whenFindById() {
+        val tracker = Tracker()
+        tracker.add(Item(0, "ItemName"))
+        assertEquals(tracker.findById(1)?.name, "ItemName")
+    }
+
+    @Test
+    fun whenFindAll() {
+        val tracker = Tracker()
+        tracker.add(Item(0, "ItemName1"))
+        tracker.add(Item(0, "ItemName2"))
+        assertEquals(tracker.findAll().size, 2)
+    }
+}
