b3e94c0c68cc7f337fac68b5e083e82f6c04dcc2
FedorovSA <itfedorovsa@gmail.com>
2023-08-16 16:53:22 +0300

    Inheritance

---
+++ src/main/kotlin/ru/job4j/oop/inheritance/Person.kt
@@ -0,0 +1,3 @@
+package ru.job4j.oop.inheritance
+
+open class Person(val name : String)
---
+++ src/main/kotlin/ru/job4j/oop/inheritance/Student.kt
@@ -0,0 +1,3 @@
+package ru.job4j.oop.inheritance
+
+class Student(name: String, group: String) : Person(name)
