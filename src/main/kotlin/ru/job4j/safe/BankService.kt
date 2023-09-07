edceafa18cce4824192c1ddd592d9274e095717d
FedorovSA <itfedorovsa@gmail.com>
2023-09-07 12:26:08 +0300

    Let function

--- src/main/kotlin/ru/job4j/safe/BankService.kt
+++ src/main/kotlin/ru/job4j/safe/BankService.kt
@@ -1,56 +1,49 @@
 package ru.job4j.safe
 
 class BankService {
-    private val users = HashMap<User, ArrayList<Account?>>()
-    fun addUser(user: User) {
-        users.putIfAbsent(user, ArrayList())
-    }
+    private val users = HashMap<User, ArrayList<Account>>()
 
-    fun findByRequisite(passport: String, requisite: String): Account? {
-        val user = findByPassport(passport) ?: return null
-        return users[user]!!.stream()
-            .filter { account: Account? -> account!!.requisite == requisite }
-            .findFirst()
-            .orElse(null)
-    }
+    fun addUser(user: User): Boolean =
+        users.putIfAbsent(user, ArrayList()) == null
 
-    fun addAccount(passport: String, account: Account?) {
+    fun addAccount(passport: String, account: Account) {
         val user = findByPassport(passport) ?: return
         users[user]!!.add(account)
     }
 
-    fun findByPassport(passport: String): User? {
-        for (user in users.keys) {
-            if (user.passport == passport) {
-                return user
-            }
+    fun findByPassport(passport: String): User? =
+        users.keys.find { it.passport == passport }
+
+    fun findAccounts(passport: String): ArrayList<Account>? =
+        findByPassport(passport)?.let { user -> users[user] }
+
+
+    fun findByRequisite(passport: String, requisite: String): Account? =
+        findByPassport(passport)?.let { user ->
+            users[user]?.find { it.requisite == requisite }
         }
-        return null
-    }
 
     fun transferMoney(
         srcPassport: String, srcRequisite: String,
-        destPassport: String, descRequisite: String, amount: Double
+        destPassport: String, destRequisite: String, amount: Double
     ): Boolean {
         val source = findByRequisite(srcPassport, srcRequisite)
-        val dest = findByRequisite(destPassport, descRequisite)
-        val rsl = source != null && dest != null
+        val dest = findByRequisite(destPassport, destRequisite)
+        val rsl = source != null && dest != null && source.balance > amount
         if (rsl) {
-            source!!.balance = source.balance - amount
-            dest!!.balance = dest.balance + amount
+            source!!.balance -= amount
+            dest!!.balance += amount
         }
         return rsl
     }
 
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            val bank = BankService()
-            bank.addUser(User("321", "Petr Arsentev"))
-            var user = bank.findByPassport("3211")
-            println(user?.username)
-            user = bank.findByPassport("321")
-            println(user?.username)
-        }
-    }
+}
+
+fun main(args: Array<String>) {
+    val bank = BankService()
+    bank.addUser(User("321", "Petr Arsentev"))
+    var user = bank.findByPassport("3211")
+    println(user?.username)
+    user = bank.findByPassport("321")
+    println(user?.username)
 }
